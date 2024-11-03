0ae56cc6df5bf63270a682b6ef3347d7440d9df5
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-03 23:12:00 +0100

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -1,1 +1,20 @@
 
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j = 12
+e = 4.0
+t = 'is the best place to learn and practice coding!'
+# Read and save an integer, double, and String to your variables.
+k = i + j
+f = d + e
+u = s + t
+# Print the sum of both integer variables on a new line.
+print(k)
+# Print the sum of the double variables on a new line.
+print(f)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(u)
