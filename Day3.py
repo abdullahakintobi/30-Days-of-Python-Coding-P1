ff021e91816eec203c5d14dbb8806637d59b0a72
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-12-03 22:27:51 +0100

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,4 +1,8 @@
 # Will be updated soon
 for i in range(1, 1000):
-        if i/10 = 1:
-                print("This is a sample")
+    if i/10 = 1:
+            print("This is a sample")
+def notion(x, y):
+    result = (x * y)/(x + y)
+    return result
+    end
